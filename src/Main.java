4835da9a1a33fea01ebe075356165bf02bcf1b2b
SaadatAzimova <saadatazimova5@gmail.com>
2024-09-22 15:31:59 +0600

    add content to object

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Book book1 = new Book("Jamila", "Ch. Aitmatov", 285, 300);
+        Book book1 = new Book("Жамиля", "Ч.Айтматов", 285, 300);
+        Book book2 = new Book("Узак жол", "М.элебаев", 355, 565);
+        Book book3 = new Book("Ээсине кайткан роза", "Н.Кадырбеков", 400, 656);
+
         book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
 
     }
 }
