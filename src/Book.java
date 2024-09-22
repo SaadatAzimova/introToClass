960b41707600384c56622fc2ead16bc6fc9b61cb
SaadatAzimova <saadatazimova5@gmail.com>
2024-09-22 14:13:51 +0600

    add atrributes, add constructor1,add method1

--- src/Book.java
+++ src/Book.java
@@ -4,5 +4,18 @@ public class Book {
     public int price;
     public int pages;
 
+    public Book(String title, String author, int price, int pages) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.pages = pages;
+    }
+    void displayInfo() {
+        System.out.println("Book: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price + " soms");
+        System.out.println("Pages: " + pages);
+        System.out.println("--------------");
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        Book book1 = new Book("Jamila", "Ch. Aitmatov", 285, 300);
+        book1.displayInfo();
 
     }
 }
