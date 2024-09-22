f6c68daaeeae3f040dc2a77958277d94eccf4a1e
SaadatAzimova <saadatazimova5@gmail.com>
2024-09-22 17:29:43 +0600

    add constructor 3,4

--- src/Book.java
+++ src/Book.java
@@ -14,15 +14,31 @@ public class Book {
         this.title = title;
         this.author = author;
         this.price = price;
-
     }
-
+    public Book(String title,  int price) {
+        this.title = title;
+        this.price = price;
+    }
+    public Book() {
+        this.title = "";
+        this.author = "";
+        this.price = 0;
+        this.pages = 0;
+    }
     void displayInfo() {
         System.out.println("Book: " + title);
         System.out.println("Author: " + author);
         System.out.println("Price: " + price + " soms");
         System.out.println("Pages: " + pages);
         System.out.println("--------------");
+    }
+    void discountPrice(int discount) {
+        if (discount > 0 && discount < price) {
+            price -= discount;
+            System.out.println("Discount:" + discount + "soms");
+        } else {
+            System.out.println("no discount");
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("Жамиля", "Ч.Айтматов", 285, 300);
-        Book book2 = new Book("Узак жол", "М.элебаев", 355, 565);
-        Book book3 = new Book("Ээсине кайткан роза", "Н.Кадырбеков", 400, 656);
-        Book book4 = new Book("Гулсары","Ч.Айтматов",300);
+        Book book2 = new Book("Узак жол", "М.элебаев", 50);
+        Book book3 = new Book("Ээсине кайткан роза",  400);
+        Book book4 = new Book();
 
         book1.displayInfo();
         book2.displayInfo();
         book3.displayInfo();
         book4.displayInfo();
+
+        book1.discountPrice(50);
+        book1.displayInfo();
+        book2.discountPrice(50);
+        book2.displayInfo();
+        book3.discountPrice(50);
+        book3.displayInfo();
+        book4.discountPrice(50);
+        book4.displayInfo();
+
 
     }
 }
